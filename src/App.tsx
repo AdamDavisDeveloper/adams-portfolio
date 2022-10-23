b99e4e30d6d1b034520a56b7d5fe2e013d3b06cc
Adam Davis <adavisdeveloper@gmail.com>
2022-10-23 17:20:53 -0400

    navbar out to own file, start styling, clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import './Globals.scss';
 
+// NavBar
+import Navbar from './Navbar';
+
 // Views
 import HomeMain from './Views/Home/Main';
 import AboutMe from './Views/AboutMe/AboutMe';
 
 // Libraries
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { gsap } from "gsap";
-
-//MUI Components
-import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
-
-// MUI Icons
-//import MenuIcon from '@mui/icons-material/Menu';
-import CottageIcon from '@mui/icons-material/Cottage';
-import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
-
-const NavBar = () => {
-  const [ navigationValue, setNavigationValue ] = useState(0);
-
-    return (
-      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation
-          showLabels
-          value={navigationValue}
-          onChange={(event, newValue) => { setNavigationValue(newValue) }}
-        >
-          <BottomNavigationAction 
-            key={0} 
-            label="Home"      
-            component={Link}
-            to='/'
-            value="home"
-            icon={<CottageIcon />} 
-          />
-
-          <BottomNavigationAction 
-            key={1} 
-            label="Projects"  
-            component={Link}
-            to='/projects'
-            value="projects"
-            icon={<DeveloperModeIcon />}
-          />
-          <BottomNavigationAction 
-            key={2} 
-            label="About Me"  
-            component={Link}
-            to='/about'
-            value="about"
-            icon={<SentimentSatisfiedAltIcon />} 
-          />
-        </BottomNavigation>
-      </Paper>
-    )
-}
 
 export default function App() {
   return (
    <>
     <BrowserRouter>
-      <NavBar/>
+      <Navbar/>
       <Routes>
           <Route path='/' element={<HomeMain />} />
           <Route path="about" element={<AboutMe />} />
---
+++ src/Navbar.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+
+//MUI Components
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+
+// MUI Icons
+//import MenuIcon from '@mui/icons-material/Menu';
+import CottageIcon from '@mui/icons-material/Cottage';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+
+import { Link } from "react-router-dom";
+
+export default function() {
+  const [ navigationValue, setNavigationValue ] = useState('home');
+
+  const navActionStyles = {
+    color: "white",
+    '& .Mui-selected.MuiSvgIcon-root': {
+      color: '#C28854',
+    }
+  }
+
+    return (
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          sx={{background: "#1a1a23"}}
+          showLabels
+          value={navigationValue}
+          onChange={(event, newValue) => { setNavigationValue(newValue) }}
+        >
+          <BottomNavigationAction 
+            sx={navActionStyles}
+            key={0} 
+            label="Home"      
+            component={Link}
+            to='/'
+            value="home"
+            icon={<CottageIcon />} 
+          />
+
+          <BottomNavigationAction 
+            sx={navActionStyles}
+            key={1} 
+            label="Projects"  
+            component={Link}
+            to='/projects'
+            value="projects"
+            icon={<DeveloperModeIcon />}
+          />
+          <BottomNavigationAction 
+            sx={navActionStyles}
+            key={2} 
+            label="About Me"  
+            component={Link}
+            to='/about'
+            value="about"
+            icon={<SentimentSatisfiedAltIcon />} 
+          />
+        </BottomNavigation>
+      </Paper>
+    )
+}
