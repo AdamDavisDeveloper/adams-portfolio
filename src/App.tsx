71ba2e9f2054f9c16c6b85cbd96b1bfc9a826181
Adam Davis <adavisdeveloper@gmail.com>
2022-10-23 19:43:27 -0400

    add new paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './Globals.scss';
 
 // NavBar
@@ -7,6 +7,8 @@ import Navbar from './Navbar';
 // Views
 import HomeMain from './Views/Home/Main';
 import AboutMe from './Views/AboutMe/AboutMe';
+import Projects from './Views/Projects/Projects';
+import ProjectPage from './Views/Projects/ProjectPage';
 
 // Libraries
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,8 +19,12 @@ export default function App() {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-          <Route path='/' element={<HomeMain />} />
-          <Route path="about" element={<AboutMe />} />
+          <Route path='/'               element={<HomeMain />} />
+
+          <Route path="projects"        element={<Projects />} />
+          <Route path="projects/:slug"  element={<ProjectPage />} />
+
+          <Route path="about"           element={<AboutMe />} />
       </Routes>
     </BrowserRouter>
    </>
