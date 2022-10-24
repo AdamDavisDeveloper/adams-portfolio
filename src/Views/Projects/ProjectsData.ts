97540bf55f87da880eb0739405a23963ec591b00
Adam Davis <adavisdeveloper@gmail.com>
2022-10-24 19:25:41 -0400

    more arrow changes + projects data

--- src/Views/Projects/ProjectsData.ts
+++ src/Views/Projects/ProjectsData.ts
@@ -11,28 +11,34 @@ export interface projectTileTypes {
     tags            :projectTag[]
 }
 
+const tags = {
+    javascript: {name: "Javascript",    color: "rgba(223, 217, 61, 0.58)"},
+    generative: {name: "Generative",    color: "#234D5A"},
+    algorithm:  {name: "Algorithm",     color: "#5A2330"},
+    learning:   {name: "Learning",      color: "#315E4B"}
+}
+
 const ProjectData: projectTileTypes[] = [
     {
-        title: "See Something New",
-        description: "guaranteed to be shown what no one has ever seen or will see again.",
-        urlSlug: "see-something-new",
+        title: "Professional Portfolio",
+        description: "hey look, you're on this website right now!",
+        urlSlug: "this-website",
         imagePath: "",
-        tags: [
-            {name: "Javascript",    color: "rgba(223, 217, 61, 0.58)"},
-            {name: "Generative",    color: "#234D5A"},
-            {name: "Algorithm",      color: "#5A2330"},
-        ],
+        tags: [ tags.javascript, tags.generative, tags.algorithm ],
     },
     {
         title: "See Something New",
         description: "guaranteed to be shown what no one has ever seen or will see again.",
         urlSlug: "see-something-new",
         imagePath: "",
-        tags: [
-            {name: "Javascript",    color: "rgba(223, 217, 61, 0.58)"},
-            {name: "Generative",    color: "#234D5A"},
-            {name: "Algorithm",      color: "#5A2330"},
-        ],
+        tags: [ tags.javascript, tags.generative, tags.algorithm ],
+    },
+    {
+        title: "Korean Flashcards",
+        description: "10,000 generated flash cards to help you study Sino and Native Korean numbers.",
+        urlSlug: "korean-flashcards",
+        imagePath: "",
+        tags: [ tags.generative, tags.algorithm, tags.learning ],
     }
 ]
 
