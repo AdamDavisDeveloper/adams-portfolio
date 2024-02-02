4ea942e4ac8f83c7c7198b8d0dcdd84faee2d348
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2024-02-02 02:52:04 -0500

    fonts fixed in production (vite handles static assets strangely)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@fonts': path.resolve(__dirname, './src/assets/fonts')
+    }
+  }
 })
